18c88640ec54e88586de72c3d118313ad1327acc
Zubin Bhaumik <113847258+ZBhaumik@users.noreply.github.com>
2024-07-14 12:58:08 -0500

    DQN implemented
    Not checked, currently testing out with cartpole.

---
+++ dqn.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from copy import deepcopy
+import numpy as np
+
+class DQN:
+    def __init__(self, state_size, action_size, gamma, tau, lr):
+        self.model = nn.Sequential(
+            nn.Linear(state_size, 32),
+            nn.ReLU(),
+            nn.Linear(32, 32),
+            nn.ReLU(),
+            nn.Linear(32, action_size)
+        ).to(self.get_device())
+        
+        self.target_model = deepcopy(self.model).to(self.get_device())
+        self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
+        self.state_size = state_size
+        self.action_size = action_size
+
+        self.gamma = gamma
+        self.tau = tau
+
+    def soft_update(self):
+        for target_param, param in zip(self.target_model.parameters(), self.model.parameters()):
+            target_param.data.copy_((1 - self.tau) * target_param.data + self.tau * param.data)
+
+    def get_device(self):
+        return "cuda" if torch.cuda.is_available() else "cpu"
+
+    def act(self, state, epsilon):
+        if np.random.rand() <= epsilon:
+            return np.random.randint(0, self.action_size)
+        with torch.no_grad():
+            state = torch.as_tensor(state, dtype=torch.float).to(self.get_device())
+            return torch.argmax(self.model(state)).item()
+
+    def update(self, batch, weights=None):
+        states, actions, rewards, next_states, dones = batch
+
+        Q_s_1 = self.target_model(next_states).detach().max(1).values() # Compute the target network Q-function on s_{t+1}
+        Q_target = rewards + (self.gamma * Q_s_1 * (1 - dones)) # Add the reward, to create the target
+        Q_expected = self.model(states).gather(1, actions)
+
+        loss = nn.functional.mse_loss(Q_expected, Q_target)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+        with torch.no_grad():
+            self.soft_update()
+
+        return loss.item()
